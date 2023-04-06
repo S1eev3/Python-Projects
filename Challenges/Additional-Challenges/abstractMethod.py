b6f8cb44b564b8008add38fc23f0703156f093c9
Mitchal Larson <76798028+S1eev3@users.noreply.github.com>
2023-04-06 17:04:58 -0400

    Update abstractMethod.py

--- Challenges/Additional-Challenges/abstractMethod.py
+++ Challenges/Additional-Challenges/abstractMethod.py
@@ -1,14 +1,14 @@
 from abc import ABC, abstractmethod
-class car(ABC):
+class card(ABC):
     def paySlip(self, amount):
-        print("Your purchase amount: ", amount)
+        print("Purchase amount: ", amount)
 # This function is stating to pass an argument but we won't tell how
 # or what type of data it will be.
     @abstractmethod
     def payment(self, amount):
         pass
 
-class DebitCardPayment(car):
+class DebitCardPayment(card):
 # Defining how to implement the payment function from its parent paySlip class
     def payment(self, amount):
         print("Your purchase amount of {} exceeded your $200 limit ".format(amount))
